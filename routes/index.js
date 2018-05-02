65d2bf27d0fb5c2e75240f8b9cdb1095ed997757
avivharuzi <avivharuzi@gmail.com>
2018-05-02 17:42:30 +0300

    added person route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const movieRoute = require('./movie.route');
 const tvRoute = require('./tv.route');
+const personRoute = require('./person.route');
 const searchRoute = require('./search.route');
 
 module.exports = app => {
     app.use('/api/movie', movieRoute);
     app.use('/api/tv', tvRoute);
+    app.use('/api/person', personRoute);
     app.use('/api/search', searchRoute);
 };
