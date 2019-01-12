a15f4d4882ec278ddc484c76fe2e9d9e1185177c
avivharuzi <avivharuzi@gmail.com>
2019-01-12 12:18:01 +0200

    Add angular services

---
+++ client/src/app/services/movies.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+
+import { HttpService } from './http.service';
+
+import { BASE_API_URL_MOVIES_DISCOVER, BASE_API_URL_MOVIES_NOW_PLAYING, BASE_API_URL_MOVIES_POPULAR, BASE_API_URL_MOVIES_TOP_RATED, BASE_API_URL_MOVIES_UPCOMING, BASE_API_URL_MOVIES } from '../constants/urls';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+  constructor(
+    private httpService: HttpService
+  ) { }
+
+  getDiscover() {
+    return this.httpService.get(BASE_API_URL_MOVIES_DISCOVER);
+  }
+
+  getNowPlaying(page: number) {
+    return this.httpService.get(BASE_API_URL_MOVIES_NOW_PLAYING, {
+      params: {
+        page: page
+      }
+    });
+  }
+
+  getPopular(page: number) {
+    return this.httpService.get(BASE_API_URL_MOVIES_POPULAR, {
+      params: {
+        page: page
+      }
+    });
+  }
+
+  getTopRated(page: number) {
+    return this.httpService.get(BASE_API_URL_MOVIES_TOP_RATED, {
+      params: {
+        page: page
+      }
+    });
+  }
+
+  getUpcoming(page: number) {
+    return this.httpService.get(BASE_API_URL_MOVIES_UPCOMING, {
+      params: {
+        page: page
+      }
+    });
+  }
+
+  getDetails(id: number) {
+    return this.httpService.get(`${BASE_API_URL_MOVIES}/${id}`);
+  }
+
+  getSimilar(id: number, page: number) {
+    return this.httpService.get(`${BASE_API_URL_MOVIES}/${id}/similar`, {
+      params: {
+        page: page
+      }
+    });
+  }
+}
---
+++ client/src/app/services/tv-shows.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+
+import { HttpService } from './http.service';
+
+import { BASE_API_URL_TV_SHOWS_DISCOVER, BASE_API_URL_TV_SHOWS_POPULAR, BASE_API_URL_TV_SHOWS_TOP_RATED, BASE_API_URL_TV_SHOWS_AIRING_TODAY, BASE_API_URL_TV_SHOWS_ON_THE_AIR, BASE_API_URL_TV_SHOWS } from '../constants/urls';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TvShowsService {
+  constructor(
+    private httpService: HttpService
+  ) { }
+
+  getDiscover() {
+    return this.httpService.get(BASE_API_URL_TV_SHOWS_DISCOVER);
+  }
+
+  getAiringToday(page: number) {
+    return this.httpService.get(BASE_API_URL_TV_SHOWS_AIRING_TODAY, {
+      params: {
+        page: page
+      }
+    });
+  }
+
+  getOnTheAir(page: number) {
+    return this.httpService.get(BASE_API_URL_TV_SHOWS_ON_THE_AIR, {
+      params: {
+        page: page
+      }
+    });
+  }
+
+  getPopular(page: number) {
+    return this.httpService.get(BASE_API_URL_TV_SHOWS_POPULAR, {
+      params: {
+        page: page
+      }
+    });
+  }
+
+  getTopRated(page: number) {
+    return this.httpService.get(BASE_API_URL_TV_SHOWS_TOP_RATED, {
+      params: {
+        page: page
+      }
+    });
+  }
+
+  getDetails(id: number) {
+    return this.httpService.get(`${BASE_API_URL_TV_SHOWS}/${id}`);
+  }
+
+  getSimilar(id: number, page: number) {
+    return this.httpService.get(`${BASE_API_URL_TV_SHOWS}/${id}/similar`, {
+      params: {
+        page: page
+      }
+    });
+  }
+
+  getSeason(id: number, season: number) {
+    return this.httpService.get(`${BASE_API_URL_TV_SHOWS}/${id}/season/${season}`);
+  }
+
+  getEpisode(id: number, season: number, episode: number) {
+    return this.httpService.get(`${BASE_API_URL_TV_SHOWS}/${id}/season/${season}/episode/${episode}`);
+  }
+}
