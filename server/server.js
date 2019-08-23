4dbb17656155bae576376f6f1331bbb747ff0fb7
Aviv Haruzi <avivharuzi@gmail.com>
2019-08-23 10:09:24 +0300

    Update server port from 3000 to 8080

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const app = require('./app');
 
 const hostname = process.env.SERVER_HOSTNAME || '127.0.0.1';
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.SERVER_PORT || 8080;
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}`);
