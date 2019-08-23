8be96f87d620f3908325353fc89cd9768a3b7f47
Aviv Haruzi <avivharuzi@gmail.com>
2019-08-23 18:14:10 +0300

    Add core module with home and not found components and also update app routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './core/components/home/home.component';
+import { NotFoundComponent } from './core/components/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'movies', loadChildren: () => import('./movies/movies.module').then(mod => mod.MoviesModule) },
+  { path: 'tv', loadChildren: () => import('./tv/tv.module').then(mod => mod.TvModule) },
+  { path: 'people', loadChildren: () => import('./people/people.module').then(mod => mod.PeopleModule) },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, scrollPositionRestoration: 'enabled' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
