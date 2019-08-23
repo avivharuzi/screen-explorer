fd3240d69f5db0fcd274608d7ed29ebac7e790d7
Aviv Haruzi <avivharuzi@gmail.com>
2019-08-23 17:59:31 +0300

    Add image pipe

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,6 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    SharedModule,
     AppRoutingModule
   ],
   providers: [],
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  baseApiUrl: 'http://localhost:8080/api/v1'
+  baseApiUrl: 'http://localhost:8080/api/v1',
+  baseImagesUrl: 'https://image.tmdb.org/t/p'
 };
 
 /*
