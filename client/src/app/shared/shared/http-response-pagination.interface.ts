156ba82644d814b8088f234d8a7f62bb0fc4b230
Aviv Haruzi <avivharuzi@gmail.com>
2019-08-23 12:37:14 +0300

    Add HttpResponsePagination interface

---
+++ client/src/app/shared/shared/http-response-pagination.interface.ts
@@ -0,0 +1,10 @@
+export interface HttpResponsePagination<T> {
+  dates ?: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: T[];
+  total_results: number;
+  total_pages: number;
+}
