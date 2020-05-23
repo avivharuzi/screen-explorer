672d1d46360fa482b1131ac73bbf41576a240b9d
Aviv Haruzi <avivharuzi@gmail.com>
2020-05-23 13:14:03 +0300

    Update TMDB to work with Redis cache server

--- server/src/modules/TMDB/TMDB.ts
+++ server/src/modules/TMDB/TMDB.ts
@@ -1,6 +1,7 @@
 import got from 'got';
 
 import config from '../../config';
+import { Redis } from '../Shared/Redis';
 
 export class TMDB {
   private static readonly SORT_MOVIE_OPTIONS = [
@@ -28,77 +29,123 @@ export class TMDB {
     'popularity.asc',
   ];
 
-  static async getData(uri: string, query: { [key: string]: any } = {}, extraQuery: { [key: string]: any } = {}): Promise<any> {
+  /* eslint-disable @typescript-eslint/camelcase */
+  static async getData(
+    uri: string,
+    // eslint-disable-next-line
+    query: { [key: string]: any } = {},
+    // eslint-disable-next-line
+    extraQuery: { [key: string]: any } = {},
+    useCache = true,
+    // eslint-disable-next-line
+  ): Promise<any> {
     const searchParams = {
+      // Common query params...
       api_key: config.tmdb.api.key,
       language: query.language ? query.language : config.tmdb.api.defaultLanguage,
       page: query.page ? query.page : 1,
       region: query.region ? query.region : config.tmdb.api.defaultRegion,
+      // Dynamic query params...
       ...extraQuery,
     };
 
-    const res = await got(`${config.tmdb.api.url}/${uri}`, { searchParams });
+    const url = `${config.tmdb.api.url}/${uri}`; // Full request url.
+    const cacheKey = `${url}${JSON.stringify(searchParams)}`; // Make a unique key by url and search params.
 
-    return res.body;
+    try {
+      if (useCache) {
+        // eslint-disable-next-line
+        const cacheValue = await Redis.getInstance().get<any>(cacheKey);
+
+        if (cacheValue) {
+          return cacheValue;
+        }
+      }
+    } catch (err) {
+      // If there was an error in getting cache value do nothing...
+    }
+
+    const { body } = await got(url, { searchParams });
+
+    try {
+      if (useCache) {
+        await Redis.getInstance().set(cacheKey, body);
+        await Redis.getInstance().expire(cacheKey);
+      }
+    } catch (err) {
+      // If there was an error in setting cache key with value do nothing...
+    }
+
+    return body;
   }
 
+  /* eslint-disable @typescript-eslint/camelcase */
+
+  // eslint-disable-next-line
   static getMovieDiscoverQuery(query: { [key: string]: any }): { [key: string]: string | boolean } {
     return {
       sort_by: TMDB.SORT_MOVIE_OPTIONS.includes(query.sort_by) ? query.sort_by : '',
-      certification_country: query.certification_country ? query.certification_country : '',
-      certification: query.certification ? query.certification : '',
-      'certification.lte': query['certification.lte'] ? query['certification.lte'] : '',
-      include_adult: query.include_adult ? query.include_adult : false,
-      include_video: query.include_video ? query.include_video : false,
-      primary_release_year: query.primary_release_year ? query.primary_release_year : '',
-      'primary_release_date.gte': query['primary_release_date.gte'] ? query['primary_release_date.gte'] : '',
-      'primary_release_date.lte': query['primary_release_date.lte'] ? query['primary_release_date.lte'] : '',
-      'release_date.gte': query['release_date.gte'] ? query['release_date.gte'] : '',
-      'release_date.lte': query['release_date.lte'] ? query['release_date.lte'] : '',
-      'vote_count.gte': query['vote_count.gte'] ? query['vote_count.gte'] : '',
-      'vote_count.lte': query['vote_count.lte'] ? query['vote_count.lte'] : '',
-      'vote_average.gte': query['vote_average.gte'] ? query['vote_average.gte'] : '',
-      'vote_average.lte': query['vote_average.lte'] ? query['vote_average.lte'] : '',
-      with_cast: query.with_cast ? query.with_cast : '',
-      with_crew: query.with_crew ? query.with_crew : '',
-      with_companies: query.with_companies ? query.with_companies : '',
-      with_genres: query.with_genres ? query.with_genres : '',
-      with_keywords: query.with_keywords ? query.with_keywords : '',
-      with_people: query.with_people ? query.with_people : '',
-      year: query.year ? query.year : '',
+      certification_country: query.certification_country ?? '',
+      certification: query.certification ?? '',
+      'certification.lte': query['certification.lte'] ?? '',
+      include_adult: query.include_adult ?? false,
+      include_video: query.include_video ?? false,
+      primary_release_year: query.primary_release_year ?? '',
+      'primary_release_date.gte': query['primary_release_date.gte'] ?? '',
+      'primary_release_date.lte': query['primary_release_date.lte'] ?? '',
+      'release_date.gte': query['release_date.gte'] ?? '',
+      'release_date.lte': query['release_date.lte'] ?? '',
+      'vote_count.gte': query['vote_count.gte'] ?? '',
+      'vote_count.lte': query['vote_count.lte'] ?? '',
+      'vote_average.gte': query['vote_average.gte'] ?? '',
+      'vote_average.lte': query['vote_average.lte'] ?? '',
+      with_cast: query.with_cast ?? '',
+      with_crew: query.with_crew ?? '',
+      with_companies: query.with_companies ?? '',
+      with_genres: query.with_genres ?? '',
+      with_keywords: query.with_keywords ?? '',
+      with_people: query.with_people ?? '',
+      year: query.year ?? '',
       without_genres: query.without_genres ? query.without_genres : '',
-      'with_runtime.gte': query['with_runtime.gte'] ? query['with_runtime.gte'] : '',
-      'with_runtime.lte': query['with_runtime.lte'] ? query['with_runtime.lte'] : '',
-      with_release_type: query.with_release_type ? query.with_release_type : '',
-      with_original_language: query.with_original_language ? query.with_original_language : '',
-      without_keywords: query.without_keywords ? query.without_keywords : '',
+      'with_runtime.gte': query['with_runtime.gte'] ?? '',
+      'with_runtime.lte': query['with_runtime.lte'] ?? '',
+      with_release_type: query.with_release_type ?? '',
+      with_original_language: query.with_original_language ?? '',
+      without_keywords: query.without_keywords ?? '',
     };
   }
 
+  /* eslint-disable @typescript-eslint/camelcase */
+
+  // eslint-disable-next-line
   static getTvDiscoverQuery(query: { [key: string]: any }): { [key: string]: string | boolean } {
     return {
-      sort_by: TMDB.SORT_TV_OPTIONS.includes(query.sort_by) ? query.sort_by : '',
-      'air_date.gte': query['air_date.gte'] ? query['air_date.gte'] : '',
-      'air_date.lte': query['air_date.lte'] ? query['air_date.lte'] : '',
-      'first_air_date.gte': query['first_air_date.gte'] ? query['first_air_date.gte'] : '',
-      'first_air_date.lte': query['first_air_date.lte'] ? query['first_air_date.lte'] : '',
-      'first_air_date_year': query['first_air_date_year'] ? query['first_air_date_year'] : false,
-      timezone: query.timezone ? query.timezone : 'America/New_York',
-      'vote_average.gte': query['vote_average.gte'] ? query['vote_average.gte'] : '',
-      'vote_count.gte': query['vote_count.gte'] ? query['vote_count.gte'] : '',
-      with_genres: query.with_genres ? query.with_genres : '',
-      with_networks: query.with_networks ? query.with_networks : '',
-      without_genres: query.without_genres ? query.without_genres : '',
-      'with_runtime.gte': query['with_runtime.gte'] ? query['with_runtime.gte'] : '',
-      'with_runtime.lte': query['with_runtime.lte'] ? query['with_runtime.lte'] : '',
-      include_null_first_air_dates: query.include_null_first_air_dates ? query.include_null_first_air_dates : false,
-      // eslint-disable-next-line
-      with_original_language: query.with_original_language ? query.with_original_language : '',
-      without_keywords: query.without_keywords ? query.without_keywords : '',
-      // eslint-disable-next-line
-      screened_theatrically: query.screened_theatrically ? query.screened_theatrically : '',
-      // eslint-disable-next-line
-      with_companies: query.with_companies ? query.with_companies : '',
+      sort_by: TMDB.SORT_TV_OPTIONS.includes(query.sort_by) ?? '',
+      'air_date.gte': query['air_date.gte'] ?? '',
+      'air_date.lte': query['air_date.lte'] ?? '',
+      'first_air_date.gte': query['first_air_date.gte'] ?? '',
+      'first_air_date.lte': query['first_air_date.lte'] ?? '',
+      'first_air_date_year': query['first_air_date_year'] ?? false,
+      timezone: query.timezone ?? 'America/New_York',
+      'vote_average.gte': query['vote_average.gte'] ?? '',
+      'vote_count.gte': query['vote_count.gte'] ?? '',
+      with_genres: query.with_genres ?? '',
+      with_networks: query.with_networks ?? '',
+      without_genres: query.without_genres ?? '',
+      'with_runtime.gte': query['with_runtime.gte'] ?? '',
+      'with_runtime.lte': query['with_runtime.lte'] ?? '',
+      include_null_first_air_dates: query.include_null_first_air_dates ?? false,
+      with_original_language: query.with_original_language ?? '',
+      without_keywords: query.without_keywords ?? '',
+      screened_theatrically: query.screened_theatrically ?? '',
+      with_companies: query.with_companies ?? '',
+    };
+  }
+
+  // eslint-disable-next-line
+  static getSearchQuery(query: { [key: string]: any }): { [key: string]: string | boolean } {
+    return {
+      query: query.query ?? '',
     };
   }
 }
