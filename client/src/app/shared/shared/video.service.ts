79c365f6b8130368cb54f77cb3a5136f30cc29d6
Aviv Haruzi <avivharuzi@gmail.com>
2020-06-12 21:44:17 +0300

    Add video service

---
+++ client/src/app/shared/shared/video.service.ts
@@ -0,0 +1,37 @@
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { Injectable } from '@angular/core';
+
+import { ModalVideoComponent } from '../components/modal-video/modal-video.component';
+import { Video } from './video';
+import { VideoSite } from './video-site.enum';
+import { VideoType } from './video-type.enum';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VideoService {
+  private bsModalRef: BsModalRef;
+
+  constructor(private bsModalService: BsModalService) {
+  }
+
+  openModal(video: Video): void {
+    const initialState = {
+      video,
+    };
+
+    this.bsModalRef = this.bsModalService.show(ModalVideoComponent, { initialState, class: 'modal-video' });
+  }
+
+  getOnlyYouTubeVideos(videos: Video[]): Video[] {
+    return videos.filter(video => video.site === VideoSite.YouTube);
+  }
+
+  getYouTubeVideoTrailerOrFirstOne(videos: Video[]): Video {
+    if (videos.length === 0) {
+      return null;
+    }
+
+    return videos.find((video) => video.site === VideoSite.YouTube && video.type === VideoType.Trailer) || videos[0];
+  }
+}
