6ed2e62b4f56017420804a09f16563647d78b00d
Aviv Haruzi <avivharuzi@gmail.com>
2020-06-12 19:33:37 +0300

    Add core module with empty home and not found components

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,8 +2,18 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, HttpClientModule],
+  declarations: [
+    HomeComponent,
+    NotFoundComponent,
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+  ],
 })
-export class CoreModule {}
+export class CoreModule {
+}
