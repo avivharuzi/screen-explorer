552d6bbeec36fe163aea5e5c597b39ebfcf5b8c8
Aviv Haruzi <avivharuzi@gmail.com>
2020-06-13 11:50:04 +0300

    Add section title component

---
+++ client/src/app/shared/components/section-title/section-title.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-title',
+  templateUrl: './section-title.component.html',
+  styleUrls: ['./section-title.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SectionTitleComponent {
+}
