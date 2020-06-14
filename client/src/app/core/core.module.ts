a40249a394300c96d6bf8e9c85422ef1209b2f9e
Aviv Haruzi <avivharuzi@gmail.com>
2020-06-14 17:16:45 +0300

    Add not found interceptor

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,12 +1,21 @@
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { NotFoundInterceptor } from './shared/not-found.interceptor';
 
 @NgModule({
   declarations: [HomeComponent, NotFoundComponent],
   imports: [CommonModule, HttpClientModule],
+  providers: [
+    {
+      multi: true,
+      provide: HTTP_INTERCEPTORS,
+      useClass: NotFoundInterceptor,
+    },
+  ],
 })
 export class CoreModule {}
---
+++ client/src/app/core/shared/not-found.interceptor.ts
@@ -0,0 +1,22 @@
+import { catchError } from 'rxjs/operators';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class NotFoundInterceptor implements HttpInterceptor {
+  constructor(private router: Router) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError(err => {
+        if (err instanceof HttpErrorResponse && err.status === 404) {
+          this.router.navigateByUrl('**', { skipLocationChange: true }).then();
+        }
+
+        return throwError(err);
+      })
+    );
+  }
+}
