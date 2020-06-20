04fa030bd0a709b306b179c1666fe70a42367824
Aviv Haruzi <avivharuzi@gmail.com>
2020-06-20 18:00:16 +0300

    Update card component

--- client/src/app/shared/components/card/card.component.ts
+++ client/src/app/shared/components/card/card.component.ts
@@ -7,6 +7,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardComponent {
+  @Input() footer: string;
   @Input() poster: string;
   @Input() subtitle: string;
   @Input() title: string;
